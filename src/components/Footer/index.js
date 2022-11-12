8012c1b0f1d4b9e71c3846b7a57530932b7a6f05
hanksml <hank.small2020@gmail.com>
2022-11-12 12:44:13 -0500

    adding header and footer with hashrouter

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {   
+    return (
+    <div className="footer" id="footer">
+        <a href="https://github.com/HankSml"><img src="https://img.icons8.com/fluent/48/000000/github.png" alt="Github" className="icon"/></a>
+        <a href="https://www.linkedin.com/in/harrison-small-3bb393251/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
+        <a href="https://hanksml.github.io/coding-portfolio/"><img src="https://img.icons8.com/fluent-systems-regular/48/000000/viking-helmet.png" alt="JakeKelly.co" className="icon"/></a>
+    </div>
+    )
+}
+
+export default Footer;
